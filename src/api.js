d7ed2d293761586eca16ac5dee398374293496f9
Kritika Tripathi <kritika.tripathi03@gmail.com>
2022-08-21 15:38:24 +0530

    Made React Weather App

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1077b66976msh5d8361f8b705376p1283cajsn12addf036326',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '4363bd8e5f7749aadd0fd6ddefa5ac3f';
